8f7bc3b887a42f325490ea11ba4eeb65836af68b
SonGeon <fjen6b@gmail.com>
2015-09-18 14:07:17 +0900

    패킷 이벤트 정의 패킷 구조 변경중

--- digital_content_game/game_session.cpp
+++ digital_content_game/game_session.cpp
@@ -40,7 +40,8 @@ void game_session::handle_read(const boost::system::error_code & error)
 				&game_session::handler,
 				this,
 				asio::placeholders::error,
-				asio::placeholders::bytes_transferred));
+				asio::placeholders::
+				bytes_transferred));
 	}
 }
 
--- digital_content_game/packet_structure.h
+++ digital_content_game/packet_structure.h
@@ -1,50 +1,15 @@
 #pragma once
 #include <cstdlib>
 #include <boost/array.hpp>
+#include "packet_events.h"
 
 class Packet{
 public:
-
-	void set_body(char * str, size_t len)
-	{
-		body_size = len;
-		try {
-			std::memcpy(_data.c_array() + PACKET_HEADER, str, len);
-			std::memcpy( _data.c_array(), reinterpret_cast<char*>(&len), 4);
-		}
-		catch (std::exception& e) {
-			throw e;
-		}
-	}
-
-	const char * get_data()
-	{
-		return _data.c_array() + packet_len();
-	}
-
-	const char * get_data()
-	{
-		return _data.c_array();
-	}
-
-	size_t packet_size()
-	{
-		return body_size + PACKET_HEADER;
-	}
+	using OBJECT_IDX = unsigned int;
+	
 private:
+	PACKET_EVENT pe;
+	OBJECT_IDX ob_idx;
 
-	int packet_len()
-	{
-		int len = 0;
-		try {
-			std::memcpy(reinterpret_cast<char*>(&len), _data.c_array(), 4);
-		}
-		catch (std::exception& e) {
-			throw e;
-		}
-		return len;
-	}
-	enum : size_t {PACKET_HEADER = 4, MAX_DATA = 1024};
-	size_t	body_size;
-	boost::array<char, MAX_DATA> _data;
 };
+
