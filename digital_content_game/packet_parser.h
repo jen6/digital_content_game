fbea03c4dc3744ef897ce9504ff9efdd0730c963
SonGeon <fjen6b@gmail.com>
2015-09-27 02:23:06 +0900

    구조 변경 헤더에서 make_packet을 순수가상함수로 둠 상속받은것들에서 오버라이딩 해주기

--- digital_content_game/packet_parser.h
+++ digital_content_game/packet_parser.h
@@ -1,1 +1,8 @@
 #pragma once
+#include "packet_events.h"
+#include "packet_structure.h"
+
+namespace Packet 
+{
+	
+}
--- digital_content_game/packet_structure.h
+++ digital_content_game/packet_structure.h
@@ -13,45 +13,6 @@ namespace Packet {
 	using UINT = unsigned int;
 	enum : unsigned int { HEADER_LEN = 8, MAX_BODY_LEN = 1016 ,MAX_LEN = 1024 };
 
-	struct Header 
-	{
-		UINT packet_len;	//packet 전체 길이
-		PACKET_EVENT pe;	//packet evnet구별
-	};
-
-	struct InfoBody : public Header
-	{
-		GAME_INFO info;
-		UINT set;
-		friend class boost::serialization::access;
-
-		template<class Archive>
-		void serialize(Archive& ar, const unsigned int version)
-		{
-			ar& info;
-			ar& set;
-		}
-	};
-
-	struct StateBody : public Header
-	{
-		OBJECT_STATE state;
-		UINT object_idx;
-		UINT x, y;
-		UINT damage;
-
-		friend class boost::serialization::access;
-
-		template<class Archive>
-		void serialize(Archive& ar, const unsigned int version)
-		{
-			ar& state;
-			ar& object_idx;
-			ar& x, y;
-			ar& damage;
-		}
-	};
-
 	class Packet {
 	public:
 		char * data()
@@ -78,5 +39,63 @@ namespace Packet {
 		boost::array<char, MAX_LEN> _data;
 		UINT _size = 0;
 	};
+
+	class Header 
+	{
+	public:
+		UINT packet_len;	//packet 전체 길이
+		PACKET_EVENT pe;	//packet evnet구별
+		virtual Packet make_packet() = 0; //packet 만들기
+		Header() {}
+	protected:
+		PACKET_EVENT event = PACKET_EVENT::GAME_START;
+	};
+
+	class InfoBody : public Header
+	{
+	public:
+		InfoBody() 
+		{
+			event = PACKET_EVENT::LOAD_INFO;
+		}
+		virtual ~InfoBody(){}
+		GAME_INFO info;
+		UINT set;
+		friend class boost::serialization::access;
+
+		template<class Archive>
+		void serialize(Archive& ar, const unsigned int version)
+		{
+			ar& info;
+			ar& set;
+		}
+	};
+
+	class StateBody : public Header
+	{
+		StateBody()
+		{
+			event = PACKET_EVENT::OBJECT_STATECH;
+		}
+		virtual ~StateBody(){}
+		OBJECT_STATE state;
+		UINT object_idx;
+		UINT x, y;
+		UINT damage;
+		const PACKET_EVENT evnt = PACKET_EVENT::OBJECT_STATECH;
+
+		friend class boost::serialization::access;
+
+		template<class Archive>
+		void serialize(Archive& ar, const unsigned int version)
+		{
+			ar& state;
+			ar& object_idx;
+			ar& x, y;
+			ar& damage;
+		}
+	};
+
+
 }
 
